2c18e2eff7063a3eea62bdec2e31925a8118df4f
Valentin Schiaffino <valenschg@gmail.com>
2024-07-24 05:31:24 -0300

    elimino ruta de prolog fija

--- consultar_preguntas.py
+++ consultar_preguntas.py
@@ -3,24 +3,17 @@ import re
 
 def consultar_preguntas():
     prolog = Prolog()
-    prolog.consult("/Users/juanseijas/Library/CloudStorage/OneDrive-Personal/PERSONAL/Pancho/UCA/Ingenieria informatica/Cuarto año/Algoritmia/Final/final-algo/prolog.pl")
+    prolog.consult("prolog.pl")
     consulta =  prolog.query("preguntas_por_area(Resultado)") 
-
     dict = {}
-
-
-    for soln in prolog.query("preguntas_por_area(Resultado)") :
+    for soln in consulta :
         for i in range(len(soln['Resultado'])):
             data = soln['Resultado'][i].value
             coma = data.find(',')
             area = data[:coma][2:]
-
             preguntas = data[coma+2:-1][1:-1]
-
             pattern = r"'([^']*)'"
             matches = re.findall(pattern, preguntas)
-
             dict[area] = matches
-
     return dict
 
